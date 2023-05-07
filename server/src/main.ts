a8dfe6c6a73810df220b822c141efadd48124b4d
Dohun_Kim <raaak8128@gmail.com>
2023-05-07 19:18:36 +0900

    .

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
-import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
